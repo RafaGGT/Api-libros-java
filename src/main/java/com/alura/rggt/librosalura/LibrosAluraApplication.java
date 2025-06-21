32e50d304fa9194aeb922059e03455bcefd365fc
Rafael <rg.gallegostapia@gmail.com>
2025-06-21 15:13:16 -0400

    fix

--- src/main/java/com/alura/rggt/librosalura/LibrosAluraApplication.java
+++ src/main/java/com/alura/rggt/librosalura/LibrosAluraApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibrosAluraApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(LibrosAluraApplication.class, args);
         Menu on = new Menu();
